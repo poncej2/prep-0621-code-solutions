2eeac837a43a96ec2b97326076137fdca6586b00
poncej2 <jp92703@yahoo.com>
2021-06-14 14:20:21 -0700

    completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+
+var firstElement = 'html';
+console.log('the first element of the array is:', firstElement);
+
+var thirdElement = 'javascript';
+console.log('the 3rd element of the array is:', thirdElement);
+
+var length = 6;
+console.log('the length of the array is:', length);
+
+var lastElement = 'postgres';
+console.log('the last element of the array is:', lastElement);
