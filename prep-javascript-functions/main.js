ecb85ced050657b237252b86ba524a1de21ecfc9
poncej2 <jp92703@yahoo.com>
2021-06-16 14:19:10 -0700

    completed prep-javascript-functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,63 @@
+// completed example
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+var sum = addTwoNumbers(4, 4);
+console.log('sum', sum);
+
+// addTwoNumbers
+function addTwoNumberss(num1, num2) {
+  return num1 + num2;
+}
+console.log(addTwoNumberss(2, 2));
+
+// convertHoursToMinutes
+function convertHoursToMinutes(hours) {
+  return hours * minutesInAHour;
+}
+var minutesInAHour = 60;
+console.log(convertHoursToMinutes(2));
+
+// getGreeting
+function getGreeting(name) {
+  return hello + ' ' + name + '!';
+}
+var hello = 'Hello';
+console.log(getGreeting('World'));
+
+// addAndMultiplyBy5
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+console.log(addAndMultiplyBy5(10, 5));
+
+// multiplyAndDivideBy5
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+console.log(multiplyAndDivideBy5(35, 10));
+
+// subtractTwoNumbers
+function subtractTwoNumbers(num1, num2) {
+  return (num1 - num2);
+}
+console.log(subtractTwoNumbers(22, 7));
+
+// getCircleCircumference radius x2(diameter) x pi
+function getCircleCircumference(radius) {
+  return (radius * 2) * pi;
+}
+var pi = 3.141592653589793;
+console.log(getCircleCircumference(5));
+
+// getFullName
+function getFullName(firstName, lastName) {
+  return firstName + (' ') + lastName;
+}
+console.log(getFullName('Juan', 'Ramirez'));
+
+// cube
+function cube(number) {
+  return number * number * number;
+}
+console.log(cube(5));
