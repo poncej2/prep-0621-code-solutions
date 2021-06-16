d2cc526db00ce78e1ebef8373a975103b4718a46
poncej2 <jp92703@yahoo.com>
2021-06-16 16:34:31 -0700

    completed for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,16 @@
+// create your loops here.
+// 1st loop
+var i;
+for (i = 0; i < 10; i++) {
+  console.log(i);
+}
+// 2nd loop
+var i;
+for (i = 0; i < 10; i++) {
+  console.log(i * 2);
+}
+// 3rd loop
+var i;
+for (i = 100; i > 0; --i) {
+  console.log('Time till explosion:' + i);
+}
